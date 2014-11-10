f9bea356a01a96e4f9cd30ce316e1047341ecd0b
Sven Olderaan <admin@heaven-craft.net>
2014-11-10 13:47:17 +0100

    Tested database deploying

--- database/iproject/iproject/Scripts/Pre-Deployment/Script.PreDeployment.sql
+++ database/iproject/iproject/Scripts/Pre-Deployment/Script.PreDeployment.sql
@@ -9,3 +9,70 @@
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+
+USE iproject
+
+DECLARE @table_schema varchar(100)
+       ,@table_name varchar(100)
+       ,@constraint_schema varchar(100)
+       ,@constraint_name varchar(100)
+       ,@cmd nvarchar(200)
+ 
+ 
+--
+-- drop all the constraints
+--
+DECLARE constraint_cursor CURSOR FOR
+  select CONSTRAINT_SCHEMA, CONSTRAINT_NAME, TABLE_SCHEMA, TABLE_NAME
+    from INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+   where TABLE_NAME != 'sysdiagrams'
+   order by CONSTRAINT_TYPE asc -- FOREIGN KEY, then PRIMARY KEY
+      
+ 
+OPEN constraint_cursor
+FETCH NEXT FROM constraint_cursor INTO @constraint_schema, @constraint_name, @table_schema, @table_name
+ 
+WHILE @@FETCH_STATUS = 0 
+BEGIN
+     SELECT @cmd = 'ALTER TABLE [' + @table_schema + '].[' + @table_name + '] DROP CONSTRAINT [' + @constraint_name + ']'
+     --select @cmd
+     EXEC sp_executesql @cmd
+ 
+     FETCH NEXT FROM constraint_cursor INTO @constraint_schema, @constraint_name, @table_schema, @table_name
+END
+ 
+CLOSE constraint_cursor
+DEALLOCATE constraint_cursor
+ 
+ 
+ 
+--
+-- drop all the tables
+--
+DECLARE table_cursor CURSOR FOR
+  select TABLE_SCHEMA, TABLE_NAME
+    from INFORMATION_SCHEMA.TABLES
+   where TABLE_NAME != 'sysdiagrams'
+     and TABLE_TYPE != 'VIEW'
+ 
+OPEN table_cursor
+FETCH NEXT FROM table_cursor INTO @table_schema, @table_name
+ 
+WHILE @@FETCH_STATUS = 0 
+BEGIN
+     SELECT @cmd = 'DROP TABLE [' + @table_schema + '].[' + @table_name + ']'
+     --select @cmd
+     EXEC sp_executesql @cmd
+ 
+ 
+     FETCH NEXT FROM table_cursor INTO @table_schema, @table_name
+END
+ 
+CLOSE table_cursor 
+DEALLOCATE table_cursor
+GO
+
+CREATE TABLE Files (
+	filename	VARCHAR(64)		NOT NULL,
+	itemid		INT				NOT NULL
+);
---
+++ database/iproject/iproject/sql/release/iproject.sql
@@ -0,0 +1,179 @@
+﻿/*
+Deployment script for iproject
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "iproject"
+:setvar DefaultDataPath "e:\Program Files\Microsoft SQL Server\MSSQL10_50.SQLEXPRESS\MSSQL\DATA\"
+:setvar DefaultLogPath "e:\Program Files\Microsoft SQL Server\MSSQL10_50.SQLEXPRESS\MSSQL\DATA\"
+
+GO
+USE [master]
+
+GO
+:on error exit
+GO
+IF (DB_ID(N'$(DatabaseName)') IS NOT NULL
+    AND DATABASEPROPERTYEX(N'$(DatabaseName)','Status') <> N'ONLINE')
+BEGIN
+    RAISERROR(N'The state of the target database, %s, is not set to ONLINE. To deploy to this database, its state must be set to ONLINE.', 16, 127,N'$(DatabaseName)') WITH NOWAIT
+    RETURN
+END
+
+GO
+
+IF NOT EXISTS (SELECT 1 FROM [master].[dbo].[sysdatabases] WHERE [name] = N'$(DatabaseName)')
+BEGIN
+    RAISERROR(N'You cannot deploy this update script to target SVENLAPTOP\SQLEXPRESS. The database for which this script was built, iproject, does not exist on this server.', 16, 127) WITH NOWAIT
+    RETURN
+END
+
+GO
+
+IF (@@servername != 'SVENLAPTOP\SQLEXPRESS')
+BEGIN
+    RAISERROR(N'The server name in the build script %s does not match the name of the target server %s. Verify whether your database project settings are correct and whether your build script is up to date.', 16, 127,N'SVENLAPTOP\SQLEXPRESS',@@servername) WITH NOWAIT
+    RETURN
+END
+
+GO
+
+IF CAST(DATABASEPROPERTY(N'$(DatabaseName)','IsReadOnly') as bit) = 1
+BEGIN
+    RAISERROR(N'You cannot deploy this update script because the database for which it was built, %s , is set to READ_ONLY.', 16, 127, N'$(DatabaseName)') WITH NOWAIT
+    RETURN
+END
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ANSI_NULLS ON,
+                ANSI_PADDING ON,
+                ANSI_WARNINGS ON,
+                ARITHABORT ON,
+                CONCAT_NULL_YIELDS_NULL ON,
+                QUOTED_IDENTIFIER ON,
+                ANSI_NULL_DEFAULT ON,
+                CURSOR_DEFAULT LOCAL,
+                RECOVERY FULL 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET PAGE_VERIFY NONE 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+USE [$(DatabaseName)]
+
+GO
+/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+USE iproject
+
+DECLARE @table_schema varchar(100)
+       ,@table_name varchar(100)
+       ,@constraint_schema varchar(100)
+       ,@constraint_name varchar(100)
+       ,@cmd nvarchar(200)
+ 
+ 
+--
+-- drop all the constraints
+--
+DECLARE constraint_cursor CURSOR FOR
+  select CONSTRAINT_SCHEMA, CONSTRAINT_NAME, TABLE_SCHEMA, TABLE_NAME
+    from INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+   where TABLE_NAME != 'sysdiagrams'
+   order by CONSTRAINT_TYPE asc -- FOREIGN KEY, then PRIMARY KEY
+      
+ 
+OPEN constraint_cursor
+FETCH NEXT FROM constraint_cursor INTO @constraint_schema, @constraint_name, @table_schema, @table_name
+ 
+WHILE @@FETCH_STATUS = 0 
+BEGIN
+     SELECT @cmd = 'ALTER TABLE [' + @table_schema + '].[' + @table_name + '] DROP CONSTRAINT [' + @constraint_name + ']'
+     --select @cmd
+     EXEC sp_executesql @cmd
+ 
+     FETCH NEXT FROM constraint_cursor INTO @constraint_schema, @constraint_name, @table_schema, @table_name
+END
+ 
+CLOSE constraint_cursor
+DEALLOCATE constraint_cursor
+ 
+ 
+ 
+--
+-- drop all the tables
+--
+DECLARE table_cursor CURSOR FOR
+  select TABLE_SCHEMA, TABLE_NAME
+    from INFORMATION_SCHEMA.TABLES
+   where TABLE_NAME != 'sysdiagrams'
+     and TABLE_TYPE != 'VIEW'
+ 
+OPEN table_cursor
+FETCH NEXT FROM table_cursor INTO @table_schema, @table_name
+ 
+WHILE @@FETCH_STATUS = 0 
+BEGIN
+     SELECT @cmd = 'DROP TABLE [' + @table_schema + '].[' + @table_name + ']'
+     --select @cmd
+     EXEC sp_executesql @cmd
+ 
+ 
+     FETCH NEXT FROM table_cursor INTO @table_schema, @table_name
+END
+ 
+CLOSE table_cursor 
+DEALLOCATE table_cursor
+GO
+
+CREATE TABLE Files (
+	filename	VARCHAR(64)		NOT NULL,
+	itemid		INT				NOT NULL
+);
+
+GO
+/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+GO
