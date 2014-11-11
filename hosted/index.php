1e662f63ddc0345651400eafca7682ddabc500eb
Sven Olderaan <admin@heaven-craft.net>
2014-11-11 13:17:18 +0000

    Changed folder name

--- hosted/index.php
+++ hosted/index.php
@@ -1,10 +1,10 @@
 <html>
     <head>
         <title>Bootstrap Test</title>
-        <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.css">
-        <script src = "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src = "/bootstrap/js/bootstrap.js"></script>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
     </head>
     <body>
+        <script src = "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src = "js/bootstrap.js"></script>
     </body>
 </html>
