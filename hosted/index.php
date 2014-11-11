2559bd38d2fece9e623b4fa15e0ad6c0ec2f48a0
Sven Olderaan <admin@heaven-craft.net>
2014-11-11 13:04:54 +0000

    added bootstrap to html code

--- hosted/index.php
+++ hosted/index.php
@@ -1,7 +1,10 @@
 <html>
-  <body>
-    <p>
-      example 2
-    </p>
-  </body>
+    <head>
+        <title>Bootstrap Test</title>
+        <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.css">
+        <script src = "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src = "/bootstrap/js/bootstrap.js"></script>
+    </head>
+    <body>
+    </body>
 </html>
