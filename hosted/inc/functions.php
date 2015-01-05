5a67baccdc9ee9712bee6ce6cd406f6cb9f8544f
Sven Olderaan <admin@heaven-craft.net>
2015-01-05 14:07:43 +0100

    Added luhn's algorithm

--- hosted/inc/functions.php
+++ hosted/inc/functions.php
@@ -14,6 +14,20 @@ function isSeller($username)
     return $row['verkoper'];
 }
 
+function checkLuhn($number) {
+    settype($number, 'string');
+    $number = preg_replace("/[^0-9]/", "", $number);
+    $sumTable = array(
+        array(0,1,2,3,4,5,6,7,8,9),
+        array(0,2,4,6,8,1,3,5,7,9));
+    $sum = 0;
+    $flip = 0;
+    for ($i = strlen($number) - 1; $i >= 0; $i--) {
+        $sum += $sumTable[$flip++ & 0x1][$number[$i]];
+    }
+    return $sum % 10 === 0;
+}
+
 class DatabaseTools {
 
     static function getAuctionDurations() {
