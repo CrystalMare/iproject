78865fd7db62c27b4f18cf5531307acd5d0ab3f4
Sven Olderaan <admin@heaven-craft.net>
2015-01-06 10:33:33 +0100

    Max lengte bestand aangepast.

--- database/iproject/iproject/Schema Objects/Schemas/dbo/Tables/Bestand.table.sql
+++ database/iproject/iproject/Schema Objects/Schemas/dbo/Tables/Bestand.table.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Bestand]
 (
-	filenaam			VARCHAR(24)			NOT NULL,
+	filenaam			VARCHAR(100)			NOT NULL,
 	voorwerpnummer		INT					NOT NULL
 
-	CONSTRAINT pk_filenaam					PRIMARY KEY	(filenaam)
+	CONSTRAINT pk_filenaam					PRIMARY KEY	(filenaam),
+	CONSTRAINT fk_file_van_voorwerp FOREIGN KEY (voorwerpnummer) REFERENCES Voorwerp (voorwerpnummer)
 )
