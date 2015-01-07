e77db040b094072f11ecbe38ef5a30c558f89b10
Koen van Keulen <koenvankeulen1@hotmail.com>
2015-01-07 15:52:32 +0100

    verkoopaccount

--- hosted/pages/bevestigCode.php
+++ hosted/pages/bevestigCode.php
@@ -16,18 +16,41 @@ switch ($_SERVER['REQUEST_METHOD']) {
 function setDefaultBuffer() {
     global $buffer;
     $buffer['meldingbevestigingscode']="";
-
 }
 
 function get() {
     global $buffer;
-
 }
 
 function post() {
     global $buffer;
-    var_dump($_POST);
-    if(isset($_POST['bevestigingscodeverkoper'])){
-        $buffer['meldingbevestigingscode']="Voer alle velden in";
+
+    $buffer['meldingbevestigingscode']="";
+    if(empty($_POST['bevestigingscodeverkoper']) or empty($_POST['bank']) or empty($_POST['rekeningnummer'])){
+        $buffer['meldingbevestigingscode']="Voer alle velden in.";
+    } else if($_POST['submit'] == 'bevestig'){
+        $hash = hash('sha256', $_SESSION['username']."post");
+        if($hash == $_POST['bevestigingscodeverkoper']){
+            registrerenVerkoper($_SESSION['username'], $_POST['bank'], $_POST['rekeningnummer']);
+            verkoperverificatieVerwijder($_SESSION['username']);
+            header('Location: index.php');
+        } else {
+            $buffer['meldingbevestigingscode']="Bevestigingsode is niet correct.";
+        }
     }
+}
+
+function registrerenVerkoper($gebruiker, $bank, $rekeningnummer){
+    global $DB;
+    $sql = "INSERT INTO Verkoper (gebruikersnaam, bank, rekeningnummer, controleoptie)
+            VALUES (?, ?, ?, 'post')";
+    $params = array ($gebruiker, $bank, $rekeningnummer);
+    sqlsrv_query($DB, $sql, $params);
+}
+
+function verkoperverificatieVerwijder($gebruiker){
+    global $DB;
+    $sql = "DELETE FROM Verkoperverificatie WHERE gebruikersnaam = ?";
+    $params = array ($gebruiker);
+    sqlsrv_query($DB, $sql, $params);
 }
